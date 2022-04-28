e03316184312dfb8cd6b8e43f65d516238c82c70
22phoebeni <phoebenicholas08@gmail.com>
2022-04-28 14:44:04 -0700

    main

--- app/routes/default.py
+++ app/routes/default.py
@@ -25,3 +25,27 @@ def unit2():
 @app.route('/unit3')
 def unit3():
     return render_template('unit3.html')
+
+@app.route('/U1L1')
+def U1L1():
+    return render_template('U1L1.html')
+
+@app.route('/U1L2')
+def U1L2():
+    return render_template('U1L2.html')
+
+@app.route('/U2L1')
+def U2L1():
+    return render_template('U2L1.html')
+
+@app.route('/U2L2')
+def U2L2():
+    return render_template('U2L2.html')
+
+@app.route('/U3L1')
+def U3L1():
+    return render_template('U3L1.html')
+
+@app.route('/U3L2')
+def U3L2():
+    return render_template('U3L2.html')
